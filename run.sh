11972f20c1b13a365c92b1f1b8291e5af620a528
Lawrence Murray <lawrence@indii.org>
2016-02-08 22:36:00 +0000

    Added SMC^2 output file concatenation to run.sh. Updated init.sh script for configuration of new runs.

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-libbi sample @config.conf @prior.conf
-libbi sample @config.conf @posterior.conf
-libbi sample @config.conf @prediction.conf
+#libbi sample @config.conf @posterior.conf
+
+for i in `seq 0 3`
+do
+  ncpdq -O -a np,nr,n results/posterior.nc.$i results/posterior_t.nc.$i
+  ncks -O --mk_rec_dmn np results/posterior_t.nc.$i results/posterior_t.nc.$i
+done
+ncrcat -O results/posterior_t.nc.* results/posterior_t.nc
+ncpdq -O -a nr,n,np results/posterior_t.nc results/posterior.nc
