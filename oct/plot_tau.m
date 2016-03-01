2af1be3719025161a7965471ac655af1132f3f22
Lawrence Murray <lawrence@indii.org>
2016-03-01 11:36:42 +0000

    Resolved conflicts in plot_tau.m.

--- oct/plot_tau.m
+++ oct/plot_tau.m
@@ -1,41 +1,52 @@
-function plot_tau(base)
-  f = ncread('data/init_tau.nc', 'F');
-  X = [];
+function plot_tau(base, variable)
+  f = ncread('data/init.nc', 'F');
   F = [];
+  C = [];
 
   i = 0;
-  file = sprintf('%s.%d', base, i);
+  j = 0;
+  file = sprintf('%s.%d.%d', base, i, j);
   while exist(file, 'file')
-    X = [X; dlmread(file)];
-    F = [F; f];
+    while exist(file, 'file')
+      F = [F; f(j + 1)];
+      C = [C; double(ncread(file, variable))];
+      j = j + 1;
+      file = sprintf('%s.%d.%d', base, i, j);
+    end
     i = i + 1;
-    file = sprintf('%s.%d', base, i);
+    j = 0;
   end
-  X = X/1e6;  % convert us to s
+  if strcmp(variable, 'clock')
+    C = C/1e6;  % convert us to s
+  end
 
   % smooth mean
   inffunc = @infExact;
   meanfunc = @meanConst;
-  hyp.mean = [0.1];
+  hyp.mean = mean(C);
   covfunc = @covSEiso;
-  hyp.cov = [0.9; -3.33];
+  hyp.cov = log([1.0; 1.0]);
   likfunc = @likGauss;
-  hyp.lik = -5.0;
-  hyp = minimize(hyp, @gp, -100, inffunc, meanfunc, covfunc, likfunc, F, X)
-  [m s2] = gp(hyp, inffunc, meanfunc, covfunc, likfunc, F, X, f);
+  hyp.lik = log(var(C));
+  hyp = minimize(hyp, @gp, -100, inffunc, meanfunc, covfunc, likfunc, F, C)
+  [m s2] = gp(hyp, inffunc, meanfunc, covfunc, likfunc, F, C, f);
 
   cla;
-  plot(F, X, '.', 'color', [0.7 0.7 0.7], 'markersize', 2);
+  plot(F, C, '.', 'color', [0.5 0.5 0.5], 'markersize', 2);
   hold on;
-  plot(f, m, 'k', 'linewidth', 2);
-  plot(f, m + 1.96*sqrt(s2), 'k', 'linewidth', 1);
-  plot(f, m - 1.96*sqrt(s2), 'k', 'linewidth', 1);
+  plot(f, m, '', 'linewidth', 3, 'color', [0; 94; 184]/255);
+  %plot(f, m + 1.96*sqrt(s2), 'k', 'linewidth', 1);
+  %plot(f, m - 1.96*sqrt(s2), 'k', 'linewidth', 1);
   hold off;
 
   xlabel('F');
-  ylabel('c (s)');
-  ax = axis();
-  axis([0 ax(2) 0 ax(4)]);
+  if strcmp(variable, 'clock')
+    ylabel('c (s)');
+    ax = axis();
+    axis([0 ax(2) 0 ax(4)]);
+  else
+    ylabel('log p(y|F)');
+  end
   grid on;
   box on;
   set(gca, 'ticklength', [0 0]);
