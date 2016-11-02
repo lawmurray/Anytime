c13ba0d556668d21f83522b06cd1b5cbcc7eae11
Lawrence Murray <lawrence@indii.org>
2016-11-02 15:06:09 +0100

    Updated parameters for toy experiments.

--- oct/prepare_toy.m
+++ oct/prepare_toy.m
@@ -1,6 +1,6 @@
 function prepare_toy()
-  N = 100;
-  T = 101;
+  N = 2^18;
+  T = 201;
   alpha = 2.0;
   theta = 0.5;
   rho = 0.5;
@@ -13,19 +13,20 @@ function prepare_toy()
     dlmwrite(file, R);
 
     % K-chain uncorrected runs
-    R = zeros(4,T);
-    %for lgK = 1:size(R,1)
-    lgK = 4;
-      R(lgK,:) = toyK(2^lgK, T, N, rho, alpha, theta, p, 0, 0);
-    %end
+    R = zeros(5,T);
+    for lgK = 1:size(R,1)
+      K = 2^lgK;
+      R(lgK,:) = toyK(K, T, N/K, rho, alpha, theta, p, 0, 0);
+    end
     file = sprintf('results/toy_uncorrected_%d.csv', p);
     dlmwrite(file, R);
 
     % K-chain corrected runs
-    %for lgK = 1:size(R,1)
-    %  R(lgK,:) = toyK(2^lgK, T, N, rho, alpha, theta, p, 1, 0);
-    %end
-    %file = sprintf('results/toy_corrected_%d.csv', p);
-    %dlmwrite(file, R);
+    for lgK = 1:size(R,1)
+      K = 2^lgK;
+      R(lgK,:) = toyK(K, T, N/K, rho, alpha, theta, p, 1, 0);
+    end
+    file = sprintf('results/toy_corrected_%d.csv', p);
+    dlmwrite(file, R);
   end
 end
